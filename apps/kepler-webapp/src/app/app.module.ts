586e686f6479ec81cfe6059d83107dba2954da89
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-10-16 22:07:20 -0400

    Import libs into kepler-webapp

--- apps/kepler-webapp/src/app/app.module.ts
+++ apps/kepler-webapp/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@kepler/material'
+import { HomeModule } from './home/home.module';
+import { CustomersModule } from './customers/customers.module';
+import { ProjectsModule } from './projects/projects.module';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule,
-    MaterialModule],
+    MaterialModule,
+    HomeModule,
+    CustomersModule,
+    ProjectsModule,
+
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
